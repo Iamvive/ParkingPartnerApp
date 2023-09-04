2c13a54d8dc747fa6c9920cf458bbfe20433c42e
Vivek Kumar <vicky.chadhary67@gmail.com>
2023-09-04 23:36:20 +0530

    Gradle Plugin Config changes.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,11 +19,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
